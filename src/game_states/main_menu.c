dc181964d7aa1439c656053c49f62bd88316133a
tozpeak <tozpeak@gmail.com>
2025-07-14 14:36:55 +0300

    abstracting menu logic from main_menu

--- src/game_states/main_menu.c
+++ src/game_states/main_menu.c
@@ -4,24 +4,13 @@
 #include <raymath.h>
 
 #include <level.h>
+#include <menu.h>
 #include <game_states/main_menu.h>
 
-typedef struct
-{
-    char *text;
-    void (*action)();
-} MenuOption;
-
-typedef struct
-{
-    char *header;
-    char *subtext;
-    int menuCount;
-    MenuOption* options;
-} MenuConfig;
-
-MenuConfig *currentMenu = NULL;
-int currentOption = 0;
+MenuState mainMenuState = {
+    .menu = NULL,
+    .currentOption = 0,
+};
 
 void MenuAction_Main_StartGame();
 void MenuAction_Main_HowTo();
@@ -71,7 +60,7 @@ void MenuAction_Main_StartGame()
 
 void MenuAction_Main_HowTo()
 {
-    MenuAction_SetMenu(&howTo);
+    Menu_SetMenu(&mainMenuState, &howTo);
 }
 
 extern bool g_closeTheGame;
@@ -80,112 +69,15 @@ void MenuAction_Main_Exit()
     g_closeTheGame = true;
 }
 
-
 void MenuAction_HowTo_Back()
 {
-    MenuAction_SetMenu(&mainMenu);
-}
-
-
-void MenuAction_SetMenu(MenuConfig *newMenu)
-{
-    currentMenu = newMenu;
-    currentOption = 0;
-}
-
-int String_LineCount(char *str)
-{
-    int lineCount = 1;
-    
-    while (*str != '\0') {
-        if(*str == '\n') lineCount++;
-        str++;
-    }
-    
-    return lineCount;
-}
-
-
-
-void Menu_SelectOption()
-{
-    bool isSelectPressed = 
-        IsKeyPressed(KEY_E)
-        || IsKeyPressed(KEY_ENTER);
-    
-    if (isSelectPressed)
-    {
-        currentMenu->options[currentOption].action();
-        return; //do not process input after option is selected
-    }
-    
-    if ( IsKeyPressed(KEY_W) ) currentOption--;
-    if ( IsKeyPressed(KEY_S) ) currentOption++;
-    
-    if (currentOption < 0)
-        currentOption = 0;
-    if (currentOption >= currentMenu->menuCount)
-        currentOption = currentMenu->menuCount - 1;
-}
-
-void MainMenu_DrawMenu()
-{
-    int x, y;
-    x = 40;
-    y = 40;
-    int textHeight;
-    
-    textHeight = 32;
-    DrawText(
-        currentMenu->header,
-        x, y, textHeight, WHITE
-    );
-    y += textHeight;
-    
-    textHeight = 16;
-    char *subtext = currentMenu->subtext;
-    if (subtext != NULL) {
-        int lineCount = String_LineCount(subtext);
-        
-        DrawText(
-            subtext,
-            x, y, textHeight, GRAY
-        );
-        y += textHeight * lineCount;
-    }
-    
-    textHeight = 24;
-    for (int i=0; i<currentMenu->menuCount; i++) {
-        MenuOption *option = currentMenu->options + i;
-        bool isSelected = i == currentOption;
-        
-        DrawText(
-            option->text,
-            x, y, textHeight,
-            (isSelected) ? ORANGE : RED
-        );
-        
-        if (isSelected) {
-            DrawPoly(
-                (Vector2) {
-                    x - textHeight * 2 / 3,
-                    y + textHeight/2
-                },
-                3,
-                textHeight / 3,
-                0.f,
-                ORANGE
-            );
-        }
-        
-        y += textHeight;
-    }
+    Menu_SetMenu(&mainMenuState, &mainMenu);
 }
 
 Camera2D *camera;
 void GST_Main_OnEnter()
 {
-    MenuAction_SetMenu(&mainMenu);
+    Menu_SetMenu(&mainMenuState, &mainMenu);
     
     Vector2 centerScreenOffset = (Vector2){ g_screenSettings.width / 2.f, g_screenSettings.height / 2.f };
     camera = g_screenSettings.camera;
@@ -199,12 +91,12 @@ void GST_Main_OnLoop()
     ClearBackground(BLACK);
     
     BeginMode2D(*camera);
-    MainMenu_DrawMenu();
+    Menu_DrawMenu(&mainMenuState);
     EndMode2D();
     
     EndDrawing();
     
-    Menu_SelectOption();
+    Menu_ProcessInput(&mainMenuState);
 }
 
 GameStateConfig GST_MainMenu_Create()
---
+++ src/menu.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <raylib.h>
+#include <raymath.h>
+
+#include <level.h>
+#include <menu.h>
+
+void Menu_ProcessInput(MenuState *state)
+{
+    MenuConfig *currentMenu = state->menu;
+    bool isSelectPressed = 
+        IsKeyPressed(KEY_E)
+        || IsKeyPressed(KEY_ENTER);
+    
+    if (isSelectPressed)
+    {
+        currentMenu->options[state->currentOption].action();
+        return; //do not process input after option is selected
+    }
+    
+    if ( IsKeyPressed(KEY_W) ) state->currentOption--;
+    if ( IsKeyPressed(KEY_S) ) state->currentOption++;
+    
+    if (state->currentOption < 0)
+        state->currentOption = 0;
+    if (state->currentOption >= currentMenu->menuCount)
+        state->currentOption = currentMenu->menuCount - 1;
+}
+
+
+int String_LineCount(char *str)
+{
+    int lineCount = 1;
+    
+    while (*str != '\0') {
+        if(*str == '\n') lineCount++;
+        str++;
+    }
+    
+    return lineCount;
+}
+
+void Menu_DrawMenu(MenuState *state)
+{
+    MenuConfig *currentMenu = state->menu;
+    
+    int x, y;
+    x = 40;
+    y = 40;
+    int textHeight;
+    
+    textHeight = 32;
+    DrawText(
+        currentMenu->header,
+        x, y, textHeight, WHITE
+    );
+    y += textHeight;
+    
+    textHeight = 16;
+    char *subtext = currentMenu->subtext;
+    if (subtext != NULL) {
+        int lineCount = String_LineCount(subtext);
+        
+        DrawText(
+            subtext,
+            x, y, textHeight, GRAY
+        );
+        y += textHeight * lineCount;
+    }
+    
+    textHeight = 24;
+    for (int i=0; i<currentMenu->menuCount; i++) {
+        MenuOption *option = currentMenu->options + i;
+        bool isSelected = i == state->currentOption;
+        
+        DrawText(
+            option->text,
+            x, y, textHeight,
+            (isSelected) ? ORANGE : RED
+        );
+        
+        if (isSelected) {
+            DrawPoly(
+                (Vector2) {
+                    x - textHeight * 2 / 3,
+                    y + textHeight/2
+                },
+                3,
+                textHeight / 3,
+                0.f,
+                ORANGE
+            );
+        }
+        
+        y += textHeight;
+    }
+}
+
+void Menu_SetMenu(MenuState *state, MenuConfig *newMenu)
+{
+    state->menu = newMenu;
+    state->currentOption = 0;
+}
